aa5ab0a2ea6a44a1c5b208d74ce6393e81a2a1ae
Alan Wynn <alan.wynn@me.com>
2015-01-08 10:21:50 +0000

    add sentry config

--- application/config/sentry.php
+++ application/config/sentry.php
@@ -5,5 +5,5 @@
  *
  * Set to true and add your app client ID to enable remote error logging.
  */
-$config['sentry_enabled'] = false;
-$config['sentry_client_id'] = '';
+$config['sentry_enabled'] = ENVIRONMENT != 'development';
+$config['sentry_client_id'] = 'http://ea0130b6b3bb4982aa92aa7c59df57f9:87a8186d5ce841a0985614a60675fa1f@sentry.clouddataservice.co.uk/8';
